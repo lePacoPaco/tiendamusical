0cd102ea6d8af45e6f3612c5c09ef3fe9c92a1f5
PacoPaco <paco_was_here@hotmail.com>
2021-04-05 14:33:09 -0500

    Nombre del proyecto: TiendaMusical
    Descripción: Se agrega la vista de descripción de album.

--- tiendamusical-web/src/main/java/com/ejemplo/tiendamusicalweb/controllers/HomeController.java
+++ tiendamusical-web/src/main/java/com/ejemplo/tiendamusicalweb/controllers/HomeController.java
@@ -3,9 +3,11 @@
  */
 package com.ejemplo.tiendamusicalweb.controllers;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
@@ -15,6 +17,8 @@ import org.apache.logging.log4j.Logger;
 
 import com.ejemplo.tiendamusicalentities.dto.ArtistaAlbumDTO;
 import com.ejemplo.tiendamusicalservices.service.HomeService;
+import com.ejemplo.tiendamusicalweb.session.SessionBean;
+import com.ejemplo.tiendamusicalweb.utils.CommonUtils;
 
 /**
  * @author paco_
@@ -23,6 +27,8 @@ import com.ejemplo.tiendamusicalservices.service.HomeService;
 @ManagedBean
 @ViewScoped
 public class HomeController {
+	
+	
 	
 	/**
 	 * Objeto que permite mostrar los mensajes de LOG en la consola del servidor o en el archivo externo
@@ -38,6 +44,12 @@ public class HomeController {
 	 * Lista obtenida a partir del filtro ingresado en el buscador.
 	 */
 	private List<ArtistaAlbumDTO> artistasAlbumDTO;
+	
+	/**
+	 * Objeto que almacena informacion en sesion
+	 */
+	@ManagedProperty("#{sessionBean}")
+	private SessionBean sessionBean;
 	
 	/**
 	 * Se inyecta el objeto de Spring con JSF para obtener los métodos de lógica de negocio del home
@@ -68,6 +80,21 @@ public class HomeController {
 			});
 		}
 	}
+	
+	/**
+	 * Método que permite ver el detalle del album seleccionado por el cliente
+	 * @param artistaAlbumDTO {@link ArtistaAlbumDTO} objeto con el album seleccionado.
+	 */
+	public void verDetalleAlbum(ArtistaAlbumDTO artistaAlbumDTO) {
+		this.sessionBean.setArtistaAlbumDTO(artistaAlbumDTO);
+		try {
+			CommonUtils.redireccionar("/pages/cliente/detalle.xhtml");
+		} catch (IOException e) {
+			CommonUtils.mostrarMensaje(FacesMessage.SEVERITY_ERROR, "Ups", "Hubo un error de formato en la página a ingresar. Favor de contactar con soporte");
+			e.printStackTrace();
+		}
+	}
+	
 
 	/**
 	 * @return the filtro
@@ -109,5 +136,19 @@ public class HomeController {
 	 */
 	public void setHomeServiceImpl(HomeService homeServiceImpl) {
 		this.homeServiceImpl = homeServiceImpl;
+	}
+
+	/**
+	 * @return the sessionBean
+	 */
+	public SessionBean getSessionBean() {
+		return sessionBean;
+	}
+
+	/**
+	 * @param sessionBean the sessionBean to set
+	 */
+	public void setSessionBean(SessionBean sessionBean) {
+		this.sessionBean = sessionBean;
 	}
 }
--- tiendamusical-web/src/main/java/com/ejemplo/tiendamusicalweb/session/SessionBean.java
+++ tiendamusical-web/src/main/java/com/ejemplo/tiendamusicalweb/session/SessionBean.java
@@ -7,6 +7,7 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import com.ejemplo.tiendamusicalentities.dto.ArtistaAlbumDTO;
 import com.ejemplo.tiendamusicalentities.entities.Persona;
 
 /**
@@ -21,6 +22,11 @@ public class SessionBean {
 	 * Objeto persona que se mantendrá en la sesión
 	 */
 	private Persona persona;
+	
+	/**
+	 * Objeto que contendra la informacion del detalle del album seleccionado por el cliente
+	 */
+	private ArtistaAlbumDTO artistaAlbumDTO;
 	
 	@PostConstruct
 	public void init() {
@@ -39,6 +45,20 @@ public class SessionBean {
 	 */
 	public void setPersona(Persona persona) {
 		this.persona = persona;
+	}
+
+	/**
+	 * @return the artistaAlbumDTO
+	 */
+	public ArtistaAlbumDTO getArtistaAlbumDTO() {
+		return artistaAlbumDTO;
+	}
+
+	/**
+	 * @param artistaAlbumDTO the artistaAlbumDTO to set
+	 */
+	public void setArtistaAlbumDTO(ArtistaAlbumDTO artistaAlbumDTO) {
+		this.artistaAlbumDTO = artistaAlbumDTO;
 	}
 
 }
